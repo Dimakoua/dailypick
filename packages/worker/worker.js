a375396df3c6d8d7f378d86486ff8c2aa8896527
Dmytro Kozin <dimakoua@gmail.com>
2026-04-11 22:11:19 -0400

    rename

--- packages/worker/worker.js
+++ packages/worker/worker.js
@@ -263,6 +263,13 @@ router.get('/ballgame', (request) => {
     return Response.redirect(target.toString(), 301);
 });
 
+router.get('/apps/choices-spinner', (request) => {
+    return Response.redirect(new URL('/apps/the-wheel-spinner/', request.url).toString(), 301);
+});
+router.get('/apps/choices-spinner/', (request) => {
+    return Response.redirect(new URL('/apps/the-wheel-spinner/', request.url).toString(), 301);
+});
+
 
 export default {
     async fetch(request, env, ctx) {
